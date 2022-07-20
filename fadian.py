8104aa1218c7654d7d6f18da5b0486ccc5f0148c
ssmzhn <ssmzhn@163.com>
2022-07-20 14:24:58 +0800

    重磅更新

--- fadian.py
+++ fadian.py
@@ -1,5 +1,6 @@
 import json
 import random
+import argparse
 
 def makeText(source, obj, num=100):
     tmpList = []
@@ -11,8 +12,22 @@ def makeText(source, obj, num=100):
 def main():
     f=open("fadian.json")
     source = json.load(f)
-    name = input("请输入发病对象：")
-    textLength = int(input("请输入生成文字的长度："))
-    print(makeText(source, name, textLength))
+    if args.arguments!=None:
+        print(makeText(source, args.arguments[0], args.arguments[1]))
+    else:
+        name = input("请输入发癫对象：")
+        textLength = int(input("请输入生成文字的长度："))
+        print(makeText(source, name, textLength))
+def str_and_int(x):
+    try:
+        return int(x)
+    except:
+        return x
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a','--arguments',nargs=2,type=str_and_int,help=
+            """在命令行中传入参数，而不是运行中传入参数。
+            第一个参数表示发癫的对象，第二个参数表示字数。"""
+    )
+    args = parser.parse_args()
     main()
