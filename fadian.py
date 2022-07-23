80bfb09c923913f63b5d1ea5b8dc802e76dbf02c
ssmzhn <ssmzhn@163.com>
2022-07-23 21:49:28 +0800

    Purofle 说要用局部变量捏

--- fadian.py
+++ fadian.py
@@ -13,18 +13,18 @@ def makeText(source, obj, num=-1):
     else:
         currentText = "".join(source)
         return currentText.format(name=obj)
-def main():
+def main(argument):
     f=open("fadian.json",encoding='UTF-8')
     source = json.load(f)
-    if args.object:
+    if argument.object:
         name = args.object[0]
-        if args.length:
+        if argument.length:
             textLength = args.length[0] # 有o有l，随机语句
         else:
             textLength = -1             # 有o没l，标准模板
     else:
         name = input("请输入发癫对象：")
-        if args.length:
+        if argument.length:
             textLength = args.length[0] # 没o有l，询问名字
         else:
             textLength = int(input("请输入生成文字的长度 (输入 -1 来使用标准模板)："))# 没o没l，完全交互
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     parser.add_argument('-o','--object',nargs=1,help='在命令行传入发癫对象')
     parser.add_argument('-l','--length',nargs=1,type=int,help='在命令行传入字数 (若不选此项则使用标准模板)')
     args = parser.parse_args()
-    main()
+    main(args)
